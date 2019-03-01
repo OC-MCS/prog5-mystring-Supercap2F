a8f3df7e7aa6e68a36c5c39b07b5c1a793435d9c
NinjaFirstCoder <daniel.andresen@okcrobot.com>
2019-03-01 09:31:13 -0600

    Finished code

--- MyString/MyString.cpp
+++ MyString/MyString.cpp
@@ -4,40 +4,77 @@
 #include <cstring>
 using namespace std;
 
-// default constructor 
+//=================================================
+// Purpose: Constructor - initialize variables when the class is created
+// Parameters:
+//			none
+// Returned Value: 
+//			none
 myString::myString() {
 	cptr = nullptr;
 };
 
+//=================================================
+// Purpose: Constructor - initialize variables to whatever 
+//		is passed to the function when the class is created
+// Parameters:
+//			const char * str - string to load into the class string
+// Returned Value: 
+//			none
 myString::myString(const char *str) {
 	cptr = new char[strlen(str) + 1];
 	strcpy_s(cptr, strlen(str) + 1, str);
 };
 
-// copy constructor
+//=================================================
+// Purpose: Copy Constructor - initialize variables to a class
+//	 when the class is initialized
+// Parameters:
+//			myString - object to load into the class
+// Returned Value: 
+//			none
 myString::myString(myString &obj) {
+	cout << "Copy constructor used" << endl;
 	cptr = new char[strlen(obj.cptr) + 1];
 	strcpy_s(cptr, strlen(obj.cptr) + 1, obj.cptr);
 };
 
-// destructor 
+//=================================================
+// Purpose: Destructor - delete the dynamic memory 
+//	when the class is no longer used
+// Parameters:
+//			none
+// Returned Value: 
+//			none 
 myString::~myString() {
+	cout << "Destructor used" << endl;
 	delete [] cptr;
 };
 
-// overloaded = operator
+//=================================================
+// Purpose: overloaded = operator = allow us to assign
+// classes to each other using the equal sign
+// Parameters:
+//			myString - object we are setting this too
+// Returned Value: 
+//			myString - allows us to cascade equals like so s1 = s2 = s3
 myString myString::operator=(const myString &right) {
 	int SIZE = strlen(right.cptr) + 1;
 	if (this != &right) {
-		delete [] cptr;
+		delete[] cptr;
 		cptr = new char[SIZE];
 		strcpy_s(cptr, SIZE, right.cptr);
 	}
-
 	return *this;
 };
 
-// overloaded + operator
+//=================================================
+// Purpose: Overloaded + operator - allows us to add
+//	classes without using a function just the + sign
+// Parameters:
+//			myString - object we are adding to this
+// Returned Value: 
+//			myString - sum after the addition 
 myString myString::operator+(const myString &right) {
 	myString temp;
 	int length = strlen(right.cptr) + strlen(cptr) + 1;
@@ -50,17 +87,37 @@ myString myString::operator+(const myString &right) {
 	return temp;
 };
 
-// overloaded == operator
+
+//=================================================
+// Purpose: Overloaded == operator - allows us to compare
+//	classes without using a function just the == sign
+// Parameters:
+//			myString - object we are comparing with
+// Returned Value: 
+//			bool - true if this = right, false if not 
 bool myString::operator==(const myString &right) {
 	return !strcmp(cptr, right.cptr);
 };
 
-// getter function returns the char pointer
+//=================================================
+// Purpose: returns the string so non-friended functions
+//	can work with it.
+// Parameters:
+//			none
+// Returned Value: 
+//			char * - c string 
 char * myString::c_str() const {
 	return cptr;
 };
 
-// overloaded << operator
+//=================================================
+// Purpose: overloaded << operator - allows us to use cout
+//	with the string class
+// Parameters:
+//			strm - the stream 
+//			myString - the object to be printed 
+// Returned Value: 
+//			strm - the cout stream
 ostream &operator << (ostream &strm, const myString &obj) {
 	if (obj.c_str() == nullptr) {
 		strm << "\0";
--- MyString/testMyString.cpp
+++ MyString/testMyString.cpp
@@ -1,26 +1,48 @@
-// test driver code goes here
+//====================================================================
+// Daniel Andresen
+// Due March 1st, 2019
+// Programming 2 / Assigment #5
+// Description: MyString class
+//====================================================================
 #include "MyString.h"
 #include <iostream>
 
 using namespace std;
+void testFunc(myString s);
 
 int main() {
+
 	myString s1;
 	myString s2("Hello World!");
+	myString s4;
 
+	// initial strings
+	cout << "Initial Strings" << endl;
 	cout << "S1: " << s1 << endl;
 	cout << "S2: " << s2 << endl;
 	cout << endl;
+	
+	// strings equal
 	s1 = s2;
-
-	cout << "Strings combined" << endl;
+	cout << "S1 = S2" << endl;
 	cout << "S1: " << s1 << endl;
 	cout << "S2: " << s2 << endl;
 	cout << endl;
-	// strings added
+
+	// strings equal twice
 	myString s3;
+	s3 = s1 = s2;
+	cout << "S3 = S1 = S2" << endl;
+	cout << "S1: " << s1 << endl;
+	cout << "S2: " << s2 << endl;
+	cout << "S3: " << s3 << endl;
+	cout << endl;
+
+	// strings added
 	s3 = s1 + s2;
-	cout << "Strings added" << endl;
+	cout << "Strings added: S3 = S1 + S2" << endl;
+	cout << "S1: " << s1 << endl;
+	cout << "S2: " << s2 << endl;
 	cout << "S3: " << s3 << endl;
 	cout << endl;
 
@@ -34,8 +56,9 @@ int main() {
 	else {
 		cout << "No" << endl;
 	}
+	cout << endl;
 
-	s2 = "hello world";
+	s2 = "not equal";
 	cout << "Are the strings equal?" << endl;
 	cout << "S1: \"" << s1 << "\"" << endl;
 	cout << "S2: \"" << s2 << "\"" << endl;
@@ -45,6 +68,20 @@ int main() {
 	else {
 		cout << "No" << endl;
 	}
+	cout << endl;
+
+	// test passing to a function
+	testFunc(s1);
 
 	return 0;
+}
+
+//=================================================
+// Purpose: check if you can pass a string to a function
+// Parameters:
+//			myString - string to print
+// Returned Value: 
+//			none
+void testFunc(myString s) {
+	cout << "String passed to a function: " << s << endl;
 }
